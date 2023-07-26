f17de8454eaf0de2fe8744ddddab607a34f2ed96
ali goodini <ilainidoog2001@gmail.com>
2023-07-26 20:48:46 +0430

    dropmenu-carousel-section build

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,14 +4,21 @@ let dropdownBtn = document.querySelector(".dropdown-btn"),
     hamburgerIcon = document.querySelector(".hamburger-icon"),
     menuHamburger = document.querySelector(".menu-hamburger"),
     wArea = document.querySelector(".wArea"),
-    wItem = document.querySelector(".wItem");
+    wItem = document.querySelector(".wItem"),
+    Beauty = document.querySelectorAll(".Beauty"),
+    dropDownHover = document.querySelector(".drop-down-hover");
     
 
-    console.log(wArea);
-    console.log(wItem);
+    console.log(Beauty);
+    console.log(dropDownHover);
 
     wItem.addEventListener('click' , ()=>{
         wArea.classList.toggle("dis-flex")
+    })
+    Beauty.forEach((e)=>{
+        e.addEventListener("click",()=>{
+            dropDownHover.classList.toggle("dis-flex")
+    })
     })
     // dropdownBtn.addEventListener("click",()=>{
     //     dropdownContainer.classList.toggle("d-none")
