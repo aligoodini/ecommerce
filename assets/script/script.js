e8be72ffec5bb4488e21e14e2e1455a17214087e
ali goodini <ilainidoog2001@gmail.com>
2023-07-29 15:28:21 +0430

    Featured-Prouducts-section build

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,14 +7,9 @@ let dropdownBtn = document.querySelector(".dropdown-btn"),
     wItem = document.querySelector(".wItem"),
     Beauty = document.querySelectorAll(".Beauty"),
     dropDownHover = document.querySelector(".drop-down-hover"),
-    top2= document.querySelectorAll(".top-2"),
-    top3= document.querySelectorAll(".top-3"),
-    imgProduct = document.querySelector(".trending-products-offer-end-box-img-box");
-    productImg = document.querySelectorAll(".product-img");
+    imgProduct = document.querySelectorAll(".trending-products-offer-end-box-img-box");
 
 
-    // console.log(productImg);
-    console.log(top2);
 
     wItem.addEventListener('click' , ()=>{
         wArea.classList.toggle("dis-flex")
@@ -27,16 +22,9 @@ let dropdownBtn = document.querySelector(".dropdown-btn"),
 
 
     // Trending-Prouducts-section 
+    // Featured-Prouducts-section
 
-    imgProduct.addEventListener("mouseover",()=>{
-        top2[0].classList.add("d-block");
-        top3[0].classList.add("d-block");
-    })
-    imgProduct.addEventListener("mouseout",()=>{
-        top2[0].classList.remove("d-block");
-        top3[0].classList.remove("d-block");
-    })
-    productImg.forEach((e)=>{
+    imgProduct.forEach((e)=>{
         e.addEventListener("mouseover",()=>{
             e.children[1].classList.add("d-block");
             e.children[2].classList.add("d-block");
@@ -46,6 +34,8 @@ let dropdownBtn = document.querySelector(".dropdown-btn"),
             e.children[2].classList.remove("d-block");
         })
     })
+
+
     // dropdownBtn.addEventListener("click",()=>{
     //     dropdownContainer.classList.toggle("d-none")
     // })
